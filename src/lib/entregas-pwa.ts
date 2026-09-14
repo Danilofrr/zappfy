56dc9be4409b90c3284ab7d4bd4a7655cddde65e
Danilofrr <daniloferreiraa80@gmail.com>
2026-09-14 18:39:11 -0300

    Vincula icone 192 da Central no PWA

--- src/lib/entregas-pwa.ts
+++ src/lib/entregas-pwa.ts
@@ -2,10 +2,10 @@ export const ENTREGAS_ICON_VERSION = "v=9";
 
 export const ENTREGAS_MANIFEST_URL = `/manifest-entregas.json?${ENTREGAS_ICON_VERSION}`;
 export const ENTREGAS_VECTOR_ICON_URL = `/zappfy-entregas-icon.svg?${ENTREGAS_ICON_VERSION}`;
-export const ENTREGAS_ICON_192_URL = `/entregas-icon-512.png?${ENTREGAS_ICON_VERSION}`;
+export const ENTREGAS_ICON_192_URL = `/entregas-icon-192.png?${ENTREGAS_ICON_VERSION}`;
 export const ENTREGAS_ICON_512_URL = `/entregas-icon-512.png?${ENTREGAS_ICON_VERSION}`;
 export const ENTREGAS_APPLE_ICON_URL = `/entregas-icon-512.png?${ENTREGAS_ICON_VERSION}`;
-export const ENTREGAS_FAVICON_URL = `/entregas-icon-512.png?${ENTREGAS_ICON_VERSION}`;
+export const ENTREGAS_FAVICON_URL = `/entregas-icon-192.png?${ENTREGAS_ICON_VERSION}`;
 
 export const ENTREGAS_LAST_SLUG_KEY = "zappfy:entregas:last-slug";
 export const ENTREGAS_PWA_MARKER_KEY = "zappfy:entregas:pwa";
